91ffdcd3ec62f5952b1cca6f815fbd28e7c477bd
Daniel Walls <walls.dk@gmail.com>
2023-02-05 12:41:08 -0800

    lower vision curse

---
+++ Assets/Scripts/Effects/LowerVisibility.cs
@@ -0,0 +1,31 @@
+﻿using DefaultNamespace.VFX;
+using UnityEngine;
+
+namespace Effects
+{
+    public class LowerVisibility : Effect
+    {
+        private readonly float _amount;
+        
+        public override string Name => "Envelop";
+        
+        public override string Description => "Lowers visibility.";
+
+        public LowerVisibility(float amount)
+        {
+            _amount = amount;
+        }
+
+        public override void Initialize()
+        {
+            var blindness = Object.FindFirstObjectByType<BlindnessVFX>();
+            blindness.AddBlindness(_amount);
+        }
+
+        public override void Shutdown()
+        {
+            var blindness = Object.FindFirstObjectByType<BlindnessVFX>();
+            blindness.RemoveBlindness(_amount);
+        }
+    }
+}
