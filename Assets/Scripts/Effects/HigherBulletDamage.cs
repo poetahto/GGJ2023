80c0371c4cc35162988c53bea1519003eeddaac2
Daniel Walls <walls.dk@gmail.com>
2023-02-04 16:57:59 -0800

    fixed pivot for player

---
+++ Assets/Scripts/Effects/HigherBulletDamage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Effects
+{
+    public class HigherBulletDamage : Effect
+    {
+        public ApplyDamageOnCollision extraDamageComponent;
+        
+        public override void ApplyTo(GameObject obj)
+        {
+            if (obj.TryGetComponent(out BulletSpawner spawner))
+            {
+                spawner.bulletEffects.Add(extraDamageComponent);
+            }
+        }
+
+        public override void RemoveFrom(GameObject obj) {}
+    }
+}
