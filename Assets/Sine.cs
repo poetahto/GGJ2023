08d9741a6ae492c151d372904e94e3e4fac19904
Kain Shepard <kainkun100@gmail.com>
2023-02-05 11:02:25 -0800

    mole

--- Assets/Sine.cs
+++ Assets/Sine.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Sine : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 startPos;
+    Vector3 startRot;
+    Vector3 startScale;
+
+    public Vector3 posAmp;
+    public Vector3 rotAmp;
+    public Vector3 scaleAmp;
+    public float speed = 1;
+    public float offset = 0;
+
     void Start()
     {
-        
+        startPos = transform.localPosition;
+        startRot = transform.localEulerAngles;
+        startScale = transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.localPosition = startPos + Mathf.Sin(Time.time * speed + offset) * posAmp;
+        transform.localEulerAngles = startRot + Mathf.Sin(Time.time * speed + offset) * rotAmp;
+        transform.localScale = startScale + Mathf.Sin(Time.time * speed + offset) * scaleAmp;
     }
 }
