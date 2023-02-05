d72925444db9e87d2ec29d0e919943917e9a5f95
Daniel Walls <walls.dk@gmail.com>
2023-02-05 13:59:43 -0800

    final effects

---
+++ Assets/Scripts/Effects/IncraseBulletsPerShot.cs
@@ -0,0 +1,31 @@
+﻿namespace Effects
+{
+    public class IncreaseBulletsPerShot : Effect
+    {
+        public override string Name        => "Amass";
+        public override string Description => "You now create an addition bullet when shooting.";
+
+        private readonly int _amount;
+        
+        public IncreaseBulletsPerShot(int amount)
+        {
+            _amount = amount;
+        }
+        
+        public override void Initialize()
+        {
+            if (Player.TryGetComponent(out BulletSpawner spawner))
+            {
+                spawner.bulletsPerShot += _amount;
+            }
+        }
+
+        public override void Shutdown()
+        {
+            if (Player.TryGetComponent(out BulletSpawner spawner))
+            {
+                spawner.bulletsPerShot -= _amount;
+            }
+        }
+    }
+}
