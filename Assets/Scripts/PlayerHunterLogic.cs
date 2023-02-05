6c20fb858ffdfc35e7c31c3b0c6a9060a0c7ac8b
Carter <andrewscart@gmail.com>
2023-02-04 20:48:40 -0800

    Player Chasers come in from underground

--- Assets/Scripts/PlayerHunterLogic.cs
+++ Assets/Scripts/PlayerHunterLogic.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerHunterLogic : MonoBehaviour
 {
@@ -9,16 +10,36 @@ public class PlayerHunterLogic : MonoBehaviour
     private Transform _target;
     private Rigidbody _rigidbody;
     private BulletSpawner _spawner;
-        
+    public float spawnDelay;
+    public float spawnDistance;
+    public bool spawning = true;
     private void Awake()
     {
         _target = FindAnyObjectByType<PlayerMovement>().transform;
         _rigidbody = GetComponent<Rigidbody>();
         _spawner = GetComponent<BulletSpawner>();
+        StartCoroutine(spawnProcess());
     }
-
+    IEnumerator spawnProcess()
+    {
+        spawning = true;
+        Vector3 endPos = transform.position;
+        Vector3 startPos = transform.position +Vector3.down*spawnDistance;
+        for (float t = 0f; t < spawnDelay; t += Time.deltaTime)
+        {
+            float normalizedTime = t / spawnDelay;
+            //right here, you can now use normalizedTime as the third parameter in any Lerp from start to end
+            transform.position = Vector3.Lerp(startPos, endPos, normalizedTime);
+            yield return null;
+        }
+        spawning = false;
+    }
     private void FixedUpdate()
     {
+        if (spawning)
+        {
+            return;
+        }
         var distToTarget = _target.position - transform.position;
         var dirToTarget = distToTarget.normalized;
         _spawner.FiringDirection = dirToTarget;
