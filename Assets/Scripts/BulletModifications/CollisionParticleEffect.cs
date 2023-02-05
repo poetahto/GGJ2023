3f8ebdf3c67c39ec7b6a762d67d33f861f7b4c7c
Daniel Walls <walls.dk@gmail.com>
2023-02-05 11:53:44 -0800

    particles and fixed some issues with bullets

---
+++ Assets/Scripts/BulletModifications/CollisionParticleEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace BulletModifications
+{
+    public class CollisionParticleEffect : BulletModifier
+    {
+        private readonly GameObject _particlePrefab;
+        
+        public CollisionParticleEffect(GameObject particlePrefab)
+        {
+            _particlePrefab = particlePrefab;
+        }
+
+        public override BulletModifier Copy()
+        {
+            return new CollisionParticleEffect(_particlePrefab);
+        }
+
+        public override void OnHit(Collider col)
+        {
+            Object.Instantiate(_particlePrefab, Bullet.transform.position, Quaternion.identity);
+        }
+    }
+}
